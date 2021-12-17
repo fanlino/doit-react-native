d83a85a44d268ccbf48bd8889120948907e4e02a
fanlino <fanlino@gmail.com>
2021-12-17 17:40:46 +0900

    ch02_2 완료

---
+++ ch02/ch02_2/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App() {
+  const children = new Array(100)
+    .fill(null)
+    .map((notUsed, index) => <Text key={index}>Hello world! {index}</Text>);
+
+  return <SafeAreaView>{children}</SafeAreaView>;
+}
